9218403ee74417617cf0d96e4a7c8ee77fe9212b
Madhur Chauhan <madhur4127@gmail.com>
2019-01-09 22:50:02 +0530

    added more dataset

--- Data/generator.sh
+++ Data/generator.sh
@@ -4,12 +4,8 @@
 INPUT_DIR=$1
 OUTPUT_DIR=$2
 i=$(ls $OUTPUT_DIR -1 | wc -l)
-echo $e
-echo $INPUT_DIR
-echo $OUTPUT_DIR
 for file in ./$INPUT_DIR/*; do
 	((i++))
-	echo $file
-	magick convert $file -colorspace Gray -resize 35% -quality 90 $OUTPUT_DIR/$i.jpg
+	magick convert $file -colorspace Gray -resize 80% -quality 100 $OUTPUT_DIR/$i.jpg
 done
 
